f02bbd33e6c76e80e373383eb7b8041ad8822185
Kyryl Riabov <kyryl.ryabov@gmail.com>
2024-06-14 13:50:55 +0300

    Added support for polygon network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,17 +19,40 @@ const config: HardhatUserConfig = {
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts
+      accounts,
+      gasMultiplier: 1.2
     },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts
+      accounts,
+      gasMultiplier: 1.2,
+    },
+    amoy: {
+      url: `https://polygon-amoy.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      gasMultiplier: 1.2,
+    },
+    ethereum: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      gasMultiplier: 1.2,
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      gasMultiplier: 1.2,
     },
   },
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    // Or at https://polygonscan.com/
+    apiKey: {
+      sepolia: `${process.env.ETHERSCAN_API_KEY}`,
+      mainnet: `${process.env.ETHERSCAN_API_KEY}`,
+      polygonAmoy: `${process.env.POLYGONSCAN_API_KEY}`,
+      polygon: `${process.env.POLYGONSCAN_API_KEY}`,
+    },
   },
   mocha: {},
   solidity: {
